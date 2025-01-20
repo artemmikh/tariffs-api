26684155a3ac0b5af0a3721203ffc1f4376a7591
Михайлов Артём <artem.mikh100@gmail.com>
2025-01-20 11:56:12 +0300

    Add redirect to docs

--- app.py
+++ app.py
@@ -1,5 +1,6 @@
 import requests
 from fastapi import FastAPI
+from fastapi.responses import RedirectResponse
 
 from config import API_URL, X_WHATSAPP_TOKEN, TITLE, DESCRIPTION
 
@@ -8,6 +9,12 @@ app = FastAPI(
     description=DESCRIPTION
 )
 
+
+@app.get('/')
+def redirect_to_docs() -> RedirectResponse:
+    return RedirectResponse(url='/docs')
+
+
 @app.get("/tariffs")
 def get_tariffs(currency='RUB', crm='lk'):
     """Получение тарифов из внешнего API."""
@@ -15,4 +22,3 @@ def get_tariffs(currency='RUB', crm='lk'):
     params = {'currency': currency, 'crm': crm}
     response = requests.get(url=API_URL, headers=headers, params=params)
     return response.json()
-
